091562c97643034749c8a9ccb664735be09d6b0b
HackerU <Hackeru@Teacher.local>
2016-05-16 10:31:08 +0300

    add permissions and location manager

--- MapKitDemos/ViewController.swift
+++ MapKitDemos/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
+    
+
 
     @IBAction func mapTypeChanged(sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex{
@@ -20,20 +26,46 @@ class ViewController: UIViewController {
         case 2:
             mapView.mapType = .Hybrid
         default:
-            break            
+            break
         }
     }
-    @IBOutlet weak var mapView: MKMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupLocationManager()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupLocationManager(){
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
     }
-
-
 }
 
+extension ViewController : CLLocationManagerDelegate{
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedAlways{
+            print("Yay")
+        }
+        else if status == .Denied || status == .NotDetermined{
+            print("No permissions yet")
+        }
+    }
+}
+
+extension ViewController: MKMapViewDelegate{
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        //
+    }
+    
+    func mapView(mapView: MKMapView, didFailToLocateUserWithError error: NSError) {
+        //
+    }
+    
+    func mapViewDidFinishLoadingMap(mapView: MKMapView) {
+        //
+    }
+    
+    func mapView(mapView: MKMapView, didDeselectAnnotationView view: MKAnnotationView) {
+        //
+    }
+}
